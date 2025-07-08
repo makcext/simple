41b93b32db61873ebaa18d180bf1e5d35dfacfb3
ALINASUSHCHENKO <1132231439@pfur.ru>
2025-07-08 17:52:17 +0300

    Fixed urls

--- simple/api/library/urls.py
+++ simple/api/library/urls.py
@@ -1,10 +1,19 @@
-from django.urls import path
+from django.urls import path, re_path, include
+from rest_framework import routers
 from simple.api.library.views import AuthorListView
 
+router = routers.DefaultRouter()
+
 urlpatterns = [
-    path(
+    re_path(
         r"^api/author/$",
         AuthorListView.as_view(),
         name="author-list",
         ),
+    re_path(
+        r"^api/author/(?P<id>\d+)/$",
+        AuthorListView.as_view(),
+        name="author-detail",
+    ),
+    re_path(r"", include(router.urls)),
 ]
