78cf06e50a80059c15c111dca5194ebd9366f4b4
ALINASUSHCHENKO <1132231439@pfur.ru>
2025-07-31 20:48:55 +0300

    Added book id

--- simple/api/library/serializers/root.py
+++ simple/api/library/serializers/root.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from simple.factories.seeds.authors import Author
+from simple.factories.seeds.books import Book
 
 
 class AuthorSerializer(serializers.Serializer):
@@ -19,3 +20,19 @@ class AuthorFieldsSerializer(serializers.Serializer):
     is_active = serializers.BooleanField()
     created_at = serializers.DateTimeField()
     updated_at = serializers.DateTimeField()
+
+
+class BookSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    original_title = serializers.CharField(required=False, allow_blank=True)
+    slug = serializers.CharField(max_length=100)
+    description = serializers.CharField(required=False, allow_blank=True)
+    publication_date = serializers.DateField()
+    isbn = serializers.CharField(max_length=20)
+    page_count = serializers.IntegerField(min_value=1)
+    author = serializers.PrimaryKeyRelatedField(queryset=Author.objects.all())
+    author_name = serializers.CharField(source="author.full_name", read_only=True)
+    is_active = serializers.BooleanField(default=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
